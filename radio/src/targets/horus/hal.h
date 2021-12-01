d3bde2dcd6cf94535e3493a2504795610785ba49
rotorman <risto.koiva@web.de>
2021-12-01 22:31:58 +0100

    On DevKit Blue LED uses PB2 instead of PE5 (latter is used for ETM on DevKit).

--- radio/src/targets/horus/hal.h
+++ radio/src/targets/horus/hal.h
@@ -450,8 +450,8 @@
   #define LED_RED_GPIO_PIN              GPIO_Pin_5
   #define LED_GREEN_GPIO                GPIOE
   #define LED_GREEN_GPIO_PIN            GPIO_Pin_4
-  #define LED_BLUE_GPIO                 GPIOE
-  #define LED_BLUE_GPIO_PIN             GPIO_Pin_5
+  #define LED_BLUE_GPIO                 GPIOB
+  #define LED_BLUE_GPIO_PIN             GPIO_Pin_2
 #elif defined(PCBX10)
   #define LED_RCC_AHB1Periph            RCC_AHB1Periph_GPIOE
   #define LED_GPIO                      GPIOE
